743b6400f9e6b81dad5bbaea2d177346f79830fc
sodap <edu1337@gmail.com>
2020-08-11 02:57:12 +0200

    add background

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,17 @@
 package;
 
 import flixel.FlxState;
+import flixel.addons.display.FlxBackdrop;
 
 class PlayState extends FlxState
 {
+	var background:FlxBackdrop;
+
 	override public function create()
 	{
 		super.create();
+		background = new FlxBackdrop("assets/images/background.png", 0, -4, true, true, 0, 0);
+		add(background);
 	}
 
 	override public function update(elapsed:Float)
